703cdca2be57e5c123e17917b8a7df89c56c27ad
Dominic Hsiao <hsiaod@oregonstate.edu>
2021-06-06 18:11:39 -0700

    Fixed the liniting problems

--- test_calculator.py
+++ test_calculator.py
@@ -9,3 +9,5 @@ class TestCalculatorApp:
     def test_subtract(self):
         assert 2 == calculator.subtract(5, 3)
 
+    def test_muiltiply(self):
+        assert 20 == calculator.subtract(2, 10)
